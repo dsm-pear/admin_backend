876c52aeb7f96be1564d3eacfe387d32e42c3bb0
goeun <gogo8272@gmail.com>
2020-10-22 16:48:41 +0900

    [Modify] hidden.py

--- Pear_Admin/hidden.py
+++ Pear_Admin/hidden.py
@@ -1,8 +1,3 @@
 JWT_SECRET_KEY = 'HIDDEN'
-<<<<<<< Updated upstream
-DB_PASSWORD = 'pear-admin-testdb'
-DB_HOST = 'pear-admin-testdb.cgbmjhvcpi1q.ap-northeast-2.rds.amazonaws.com'
-=======
 DB_PASSWORD = 'pear-admin-test'
 DB_HOST = 'pear-admin-test.cgbmjhvcpi1q.ap-northeast-2.rds.amazonaws.com'
->>>>>>> Stashed changes
