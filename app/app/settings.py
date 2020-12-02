f46ec90979e9bd5778b8c8f6a54ebcbd9426eca6
GoEun <gogo8272@gmail.com>
2020-12-02 17:48:35 +0900

    [Modify] settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from app.hidden import DB_PASSWORD, DB_HOST
+from .hidden import DB_PASSWORD, DB_HOST
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
